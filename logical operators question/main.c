dd75713777a3efc7e11a5a2ff7e9fc9348497735
Mohamed Abdelwahed <memoelfares613@gmail.com>
2023-02-21 20:37:50 +0200

    after add new slides

--- logical operators question/main.c
+++ logical operators question/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int a=1,b=1,c,d;
-    c=a++||b++;
-    d=--b && a--;
+    int a=1,b=1,c,d; // a = 1  , b=2
+    c=a++||b++; //a=1 and inc next line , b =1
+    d=--b && a--; // b=0 , a = 2 
     printf("a=%i\nb=%i\nc=%i\nd=%i\n",a,b,c,d);
     return 0;
 }
--- logical operators/main.c
+++ logical operators/main.c
@@ -3,15 +3,15 @@ int main()
 {
     int x,y;
     int a = 5, b = 5, c = 10, result;
-    result = (a == b) && (c > b);
-    printf("(a == b) && (c > b) equals to %d \n", result);
-    result = (a == b) && (c < b);
-    printf("(a == b) && (c < b) equals to %d \n", result);
-    result = (a == b) || (c < b);
+    result = (a == b) && (c > b);  //  1   &&   1  =  1
+    printf("(a == b) && (c > b) equals to %d \n", result); // 1 True
+    result = (a == b) && (c < b); // 1  &&  0   =  0
+    printf("(a == b) && (c < b) equals to %d \n", result); // 0  false
+    result = (a == b) || (c < b); //1   ||  0   = 1
     printf("(a == b) || (c < b) equals to %d \n", result);
     result = (a != b) || (c < b);
     printf("(a != b) || (c < b) equals to %d \n", result);
-    result = !(a != b);
+    result = !(a != b); // 1
     printf("!(a == b) equals to %d \n", result);
     result = !(a == b);
     printf("!(a == b) equals to %d \n", result);
